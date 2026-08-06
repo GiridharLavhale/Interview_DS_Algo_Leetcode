46a7993636d86ee584f35b8e40d0e70717250691
Giridhar_Patil18 <girishlavhale@gmail.com>
2026-08-06 23:20:03 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.77%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        while(true){
+            int num = n;
+            int product = 1;
+        
+            // product of digits
+            while(num > 0){
+                int dig = num % 10;
+                product *= dig;
+                num /= 10;
+                
+            }
+            // check for divisible 
+            if(product % t ==0) 
+                return n;
+
+                n++;
+
+            
+        }        
+        
+        
+    }
+}; 
