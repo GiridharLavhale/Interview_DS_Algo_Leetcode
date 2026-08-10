2f9ed728b2bb8984c2ccc38d26f1340a629721fd
Giridhar_Patil18 <girishlavhale@gmail.com>
2026-08-10 23:59:11 +0530

    Time: 29 ms (83.73%), Space: 8.8 MB (90.65%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    vector<int> t;
-    bool solve(int n){
-        if(n == 0){
-            return false;
-        }
-        if(t[n] != -1){
-            return t[n] == 1 ? true : false;
-        }
-
-        for(int k = 1; k*k <= n; k++){
-            if(solve(n - k*k) == false )
-                return t[n] = true;
-        }
-
-
-        return t[n] = false;
-    }
     bool winnerSquareGame(int n) {
-        t.assign(n+1, -1);
-        return solve(n);
+        vector<bool> t(n+1, false);
+
+        t[0] = false; // base case 
+
+        for(int i = 1; i < n+1; i++){
+
+            for(int k = 1; k*k <= i; k++){
+                if(t[i - (k*k)] == false){
+                    t[i] = true;
+                    break;
+                }
+
+            }
+        }
+
+        return t[n]; // return solve(n)
         
     }
 };
