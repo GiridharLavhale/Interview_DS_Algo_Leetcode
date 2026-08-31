61957484a96dc3edf20e956ad3aa28cf116a85f7
Giridhar_Patil18 <girishlavhale@gmail.com>
2026-09-01 00:57:47 +0530

    Time: 5 ms (40.73%), Space: 106.3 MB (42.07%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -8,39 +8,79 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public int[] nodesBetweenCriticalPoints(ListNode head) {
+//         ListNode prev = head;
+//         ListNode curr = head.next;
+//         int i = 1;
+//         int prevcriticalpos = 0;
+//         int firstcriticalpos = 0;
+
+//         int minDist = Integer.MAX_VALUE;
+
+//         while( curr.next != null){ // Check whether curr is a critical point
+//             if(curr.val < prev.val && curr.val < curr.next.val ||
+//                 curr.val > prev.val && curr.val > curr.next.val){
+
+//                     if(prevcriticalpos == 0){
+//                         // First critical point
+//                         prevcriticalpos = i;
+//                         firstcriticalpos = i;
+
+//                     }else{  // Distance from previous critical point
+//                         minDist = Math.min(minDist, i - prevcriticalpos ); 
+//                         prevcriticalpos = i;
+//                     }
+
+//             }
+
+//             i++;
+//             prev = curr;
+//             curr = curr.next;
+//         }  // Less than two critical points
+//         if(minDist == Integer.MAX_VALUE){
+//             return new int[] {-1,-1};
+//         }
+//         // Minimum distance, maximum distance
+//         return new int[] {minDist,prevcriticalpos - firstcriticalpos };
+//     }
+// }
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        ListNode prev = head;
-        ListNode curr = head.next;
-        int i = 1;
-        int prevcriticalpos = 0;
-        int firstcriticalpos = 0;
+        int firstCriticalIndex    = 0;
+        int previousCriticalIndex = 0;
 
-        int minDist = Integer.MAX_VALUE;
+        int prevValue = 0;
+        int currValue = 0;
+        int nextValue = 0;
 
-        while( curr.next != null){
-            if(curr.val < prev.val && curr.val < curr.next.val ||
-                curr.val > prev.val && curr.val > curr.next.val){
+        int minDistance = Integer.MAX_VALUE;
+        int index = 0;
+        int[] result = {-1, -1};
 
-                    if(prevcriticalpos == 0){
-                        prevcriticalpos = i;
-                        firstcriticalpos = i;
+        while (head != null) {
+            prevValue = currValue;
+            currValue = nextValue;
+            nextValue = head.val;
 
-                    }else{
-                        minDist = Math.min(minDist, i - prevcriticalpos );
-                        prevcriticalpos = i;
-                    }
-
+            if (prevValue != 0 && currValue != 0 && nextValue != 0 && 
+                ((prevValue > currValue && currValue < nextValue) ||
+                (prevValue < currValue && currValue > nextValue))) {
+                
+                if (firstCriticalIndex == 0) {
+                    firstCriticalIndex = index;
+                } else {
+                    minDistance = Math.min(minDistance, index - previousCriticalIndex);
+                    result[0] = minDistance;
+                    result[1] = index - firstCriticalIndex;
+                }
+                previousCriticalIndex = index;
             }
 
-            i++;
-            prev = curr;
-            curr = curr.next;
-        }
-        if(minDist == Integer.MAX_VALUE){
-            return new int[] {-1,-1};
+            index++;
+            head = head.next;
         }
 
-        return new int[] {minDist,prevcriticalpos - firstcriticalpos };
+        return result;
     }
 }
